546e90e1d652ca68ed037e4237e8769f7e8675d2
valecj <valeriacossio5@gmail.com>
2018-10-25 05:28:46 -0300

    +++++++

--- public/js/regularizacionDescarga.js
+++ public/js/regularizacionDescarga.js
@@ -1,16 +1,19 @@
 function regular(){
     textoRegIrr.innerHTML= `
+    <li class="first_li"><span>holahohola</span></li>
     <li><span>holahohola</span></li>
     <li><span>holahohola</span></li>
     <li><span>holahohola</span></li>
-    <li><span>holahohola</span></li>
-    
+
+    <button>Descarga esta información</button>
 `}
 function irregular(){
     textoRegIrr.innerHTML= `
+    <li class="first_li"><span>holahoka</span></li>
     <li><span>holahoka</span></li>
     <li><span>holahoka</span></li>
     <li><span>holahoka</span></li>
-    <li><span>holahoka</span></li>
+
+    <button>Descarga esta información</button>
     
 `}
