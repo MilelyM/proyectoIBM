ebf079b9819612b437a23c848f95c9cb99160284
milelym <milelymarcial@gmail.com>
2018-10-31 19:27:12 -0300

    cambios recuperados

--- public/js/regularizacionDescarga.js
+++ public/js/regularizacionDescarga.js
@@ -1,18 +1,40 @@
-function regular(){
-    textoRegIrr.innerHTML= `
-    <li class="first_li"><span>holahohola</span></li>
-    <li><span>holahohola</span></li>
-    <li><span>holahohola</span></li>
-    <li><span>holahohola</span></li>
+function regular() {
+    textoRegIrr.innerHTML = ` 
+     <div class='container'>
+    <div class='row marginREGeIRR'>
+    <div class="col s12 left ">
+    <li><span>Dirígete al departamento de extranjeria y migración</span></li>
+    <li><span>Lleva tu ID o pasaporte + tarjeta de turismo o certificado de viajes emitido por la PDI</span></li>
+    <li><span>Debes llevar tu certificado (PER/COL/DOM) de antecedentes, de tu país de origen, en el cado de ser menor de edad se debe adjuntar el certificado de nacimiento</span></li>
+    </div>
+   </div>
+   <div class='row marginREGeIRR'>
+    <div class='col s12 '>
+    <a class="waves-effect waves-light btn-small" href="https://drive.google.com/file/d/1noLP4v8Lp0PhjmoS0Ht0r3O9QJDaGlFz/view?usp=sharing" download="siguientes_pasos">Descargar esta información</a>
+    </div>
 
-    <button>Descarga esta información</button>
-`}
-function irregular(){
-    textoRegIrr.innerHTML= `
-    <li class="first_li"><span>holahoka</span></li>
-    <li><span>holahoka</span></li>
-    <li><span>holahoka</span></li>
-    <li><span>holahoka</span></li>
+    </div>
+    </div>`
+}
 
-    <button>Descarga esta información</button>
- ` }
+function irregular() {
+    textoRegIrr.innerHTML = `
+    <div class='container'>
+
+    <div class='row marginREGeIRR'>
+    <div class="col s12  left">
+
+    <li><span>Dirígete al departamento de migraciones de la PDI</span></li>
+    <li><span>Lleva tu ID o pasaporte</span></li>
+    <li><span>Notifica tu situación actual</span></li>
+    <li><span>Comprométete a iniciar tu proceso de regularización con firma mensual</span></li>
+    </div>
+    </div>
+    <div class='row marginREGeIRR'>
+    <div class='col s12 '>
+    <a class="waves-effect waves-light btn-small" href="https://drive.google.com/file/d/13zk4r8QwgnVLVDNjI6znw3XoilY6ZAmy/view?usp=sharing" download="regularizar">Descargar esta información</a>
+    </div>
+    </div>
+    </div>
+    </div> `
+}
